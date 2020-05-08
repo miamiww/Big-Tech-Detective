b2a126feb249b17c36342dc5a3eacd67143ff1c7
miamiww <rivendalejones@gmail.com>
2020-05-07 23:39:42 -0400

    u can have a lil refactor, as a treat

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 
 const url_filter = {urls: ["<all_urls>"]}
 
+
+// exporing the web request API better
 const beforeRequest = req_details => {
 	console.log('beforeRequest')
 	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
@@ -35,12 +37,13 @@ const errorOccurred = req_details => {
 	// if(post_obj) post_obj.postMessage({'type' : 'errorOccurred' , 'req_details' : req_details});
 }
 
-chrome.webRequest.onBeforeRequest.addListener(beforeRequest, url_filter);
-chrome.webRequest.onBeforeSendHeaders.addListener(beforeSendHeaders, url_filter);
-chrome.webRequest.onHeadersReceived.addListener(headersReceived, url_filter);
+// chrome.webRequest.onBeforeRequest.addListener(beforeRequest, url_filter);
+// chrome.webRequest.onBeforeSendHeaders.addListener(beforeSendHeaders, url_filter);
+// chrome.webRequest.onHeadersReceived.addListener(headersReceived, url_filter);
 // chrome.webRequest.onCompleted.addListener(completed, url_filter);
 
 
+// this isn't really necessary, it prevents the extension from running on non-web pages like settings pages, but it is OK to make the request on such pages
 function checkForValidUrl(tabId, changeInfo, tab) {
 	chrome.pageAction.show(tab.id);
 }
@@ -48,7 +51,7 @@ function checkForValidUrl(tabId, changeInfo, tab) {
 // Listen for any changes to the URL of any tab
 // chrome.tabs.onUpdated.addListener(checkForValidUrl);
 
-// Set the item in the localstorage
+// Set the item in the localstorage - used for messaging back and forth between content and background
 function setItem(key, value) {
 	window.localStorage.removeItem(key);
 	window.localStorage.setItem(key, value);
@@ -70,11 +73,18 @@ var currentIPList	= {};
 chrome.webRequest.onCompleted.addListener( 
 	function(info) {
 	  currentIPList[ info.url ] = info.ip;
-	  console.log('completed');
-	  console.log(info);
+	//   console.log('completed');
+	//   console.log(info);
+	//	 preventing infinite loops
 	  if(info.ip !="159.65.179.9"){
 		console.log('fetch request for ' + info.ip)
-		fetch("http://159.65.179.9:8080/ips/"+info.ip).then(response => response.json()).then(data => console.log(data));
+		console.log("http://159.65.179.9:8080/ips/"+info.ip)
+		fetch("http://159.65.179.9:8080/ips/"+info.ip)
+			.then(response => response.json())
+			.then(data => {
+				console.log(data)
+			})
+			.catch(err => console.log(err));
 	  }
 	return;
   },
