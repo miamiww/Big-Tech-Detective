b50a32e937e33b6949b857a30f2553b21dcf8bb3
miamiww <rivendalejones@gmail.com>
2020-05-09 23:39:04 -0400

    adding background communication

--- app/background.js
+++ app/background.js
@@ -1,46 +1,15 @@
 
 const url_filter = {urls: ["<all_urls>"]}
+const ignore_ips = ["159.65.179.9",undefined]
 
-
-// exporing the web request API better
-const beforeRequest = req_details => {
-	console.log('beforeRequest')
-	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
-	// if(post_obj) post_obj.postMessage({'type' : 'beforeRequest' , 'req_details' : req_details});
-
-}
-
-
-const beforeSendHeaders = req_details => {
-	console.log('beforeSendHeaders')
-	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
-	// if(post_obj) post_obj.postMessage({'type' : 'beforeSendHeaders' , 'req_details' : req_details});
-}
-
-const headersReceived = req_details => {
-	console.log('headersRecieved')
-	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
-	// if(post_obj) post_obj.postMessage({'type' : 'headersReceived' , 'req_details' : req_details});
-}
-
-const completed = req_details => {
-	console.log('completed');
-	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
-	// console.log(req_details.ip)
-	// if(post_obj) post_obj.postMessage({'type' : 'completed' , 'req_details' : req_details});
-}
+let message_object = null;
+let chart_object;
 
 
 
-const errorOccurred = req_details => {
-	try{console.log(req_details); /** console.trace(); /**/ }catch(e){}
-	// if(post_obj) post_obj.postMessage({'type' : 'errorOccurred' , 'req_details' : req_details});
-}
-
-// chrome.webRequest.onBeforeRequest.addListener(beforeRequest, url_filter);
-// chrome.webRequest.onBeforeSendHeaders.addListener(beforeSendHeaders, url_filter);
-// chrome.webRequest.onHeadersReceived.addListener(headersReceived, url_filter);
-// chrome.webRequest.onCompleted.addListener(completed, url_filter);
+// try{console.log(port); /** console.trace(); /**/ }catch(e){}
+// console.assert(port.name == "start_listen");
+// message_object = port;
 
 
 // this isn't really necessary, it prevents the extension from running on non-web pages like settings pages, but it is OK to make the request on such pages
@@ -69,14 +38,11 @@ function getItem(key) {
 }
 
 // Get IP
-var currentIPList	= {};
 chrome.webRequest.onCompleted.addListener( 
-	function(info) {
-	  currentIPList[ info.url ] = info.ip;
-	//   console.log('completed');
-	//   console.log(info);
-	//	 preventing infinite loops
-	  if(info.ip !="159.65.179.9"){
+	(info) => {
+	
+	  //	 preventing infinite loops
+	  if(!ignore_ips.includes(info.ip)){
 		console.log('fetch request for ' + info.ip)
 		console.log("http://159.65.179.9:8080/ips/"+info.ip)
 		fetch("http://159.65.179.9:8080/ips/"+info.ip)
@@ -84,7 +50,12 @@ chrome.webRequest.onCompleted.addListener(
 			.then(data => {
 				console.log(data)
 				if(data.hasOwnProperty("ip")){
-					setTimeout(() => {  console.log("page loaded!"); }, 10000);	
+					//this is where we send info to the extension front-end
+					console.log('IP in database')
+					if(message_object) message_object.postMessage({'type': 'packetIn', 'company':data.ip.company})
+				}else{
+					console.log('IP not in database')
+					if(message_object) message_object.postMessage({'type': 'packetIn', 'company':'Other'})
 				}
 			})
 			.catch(err => console.log(err));
@@ -99,39 +70,14 @@ chrome.webRequest.onCompleted.addListener(
 );
 
 
-chrome.extension.onMessage.addListener(
-	function(request, sender, sendResponse)
-	{
-		switch (request.name)
-		{
-            case "setOptions":
-				// request from the content script to set the options.
-				//localStorage["websiteIP_status"] = websiteIP_status;
-				localStorage.setItem("websiteIP_status", request.status);
-			break;
-			
-			case "getOptions":
-				// request from the content script to get the options.
-				sendResponse({
-					enableDisableIP : localStorage["websiteIP_status"]
-				});
-			break;
-		    case "getIP":
-			var currentURL = sender.tab.url;
-			if (currentIPList[currentURL] !== undefined) {
-				sendResponse({
-					domainToIP: currentIPList[currentURL]
-				});
-			} else {
-				sendResponse({
-					domainToIP: null
-				});
-			}
-			
-		    break;
-		
-			default:
-			sendResponse({});
-		}
-	}
-);
+
+chrome.runtime.onConnect.addListener(function(port) {
+
+	try{console.log(port); /** console.trace(); /**/ }catch(e){}
+	console.assert(port.name == "start_listen");
+	message_object = port;
+
+
+});
+
+
